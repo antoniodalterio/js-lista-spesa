c0d9f3d56784db3f1a31de90ddf315605be014b4
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-13 16:55:56 +0100

    feat: esercizio  completo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+const spesa = [
+  'mazza',
+  'palla',
+  'guantone',
+  'caschetto',
+  'borsa',
+  'protezioni',
+  'paradenti',
+];
+let i = 0;
+const lista = document.querySelector('ul');
+
+while (i !== spesa.length) {
+  const prodotto = document.createElement('li');
+  prodotto.innerHTML = spesa[i];
+  lista.append(prodotto);
+  i++;
+}
